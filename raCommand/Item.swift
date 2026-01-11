d04206150606d6563c662e596fcd68475538d4bb
Ezra Haugabrooks <ezra@aymscores.com>
2026-01-11 15:12:39 -0500

    Initial Commit

---
+++ raCommand/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  raCommand
+//
+//  Created by E. Haugabrooks on 1/11/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ raCommandTests/raCommandTests.swift
@@ -0,0 +1,17 @@
+//
+//  raCommandTests.swift
+//  raCommandTests
+//
+//  Created by E. Haugabrooks on 1/11/26.
+//
+
+import Testing
+@testable import raCommand
+
+struct raCommandTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
